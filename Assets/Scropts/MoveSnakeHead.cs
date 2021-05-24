fab17e0eea7c9e7930bb053ef93286cc4f91236d
Sergey <59688364+Corvus-inc@users.noreply.github.com>
2021-05-24 15:17:14 +0300

    Initial commit

---
+++ Assets/Scropts/MoveSnakeHead.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSnakeHead : MonoBehaviour
+{
+    public Transform transformSnakeHead;
+
+
+    void Update()
+    {
+        transformSnakeHead.position = transformSnakeHead.position + (Vector3.right * Time.deltaTime);
+
+
+    }
+}
